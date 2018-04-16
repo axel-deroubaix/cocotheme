0b29f0da53b74ef5d6731e65eb9fbeec206aedf0
Axel Deroubaix <38394638+axel-deroubaix@users.noreply.github.com>
2018-04-16 16:03:41 +0900

    Romved useless comments

--- functions.php
+++ functions.php
@@ -1,15 +1,5 @@
 <?php
 
-/**
- * Storefront automatically loads the core CSS even if using a child theme as it is more efficient
- * than @importing it in the child theme style.css file.
- *
- * Uncomment the line below if you'd like to disable the Storefront Core CSS.
- *
- * If you don't plan to dequeue the Storefront Core CSS you can remove the subsequent line and as well
- * as the sf_child_theme_dequeue_style() function declaration.
- */
-
 /*
 ** Remove emojis
 */
