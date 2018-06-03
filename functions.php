00160937bc034d90660d0877170565318b7afbae
Axel Deroubaix <38394638+axel-deroubaix@users.noreply.github.com>
2018-06-03 18:27:55 +0900

    added tag manager

--- functions.php
+++ functions.php
@@ -24,10 +24,31 @@ function cocolo_contact_page_css() {
 
 }
 
-/*
-** Google adsense
-*/
+/* Google adsense and Tag manager*/
+add_action( 'wp_head', 'cocolo_tag_manager_header', 1);
+add_action( 'storefront_before_site', 'cocolo_tag_manager_body', 1);
 add_action( 'wp_head', 'cocolo_adsense', 999 );
+
+function cocolo_tag_manager_header() {
+	?>
+	<!-- Google Tag Manager -->
+	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+	'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+	})(window,document,'script','dataLayer','GTM-KD9K4P8');</script>
+	<!-- End Google Tag Manager -->
+	<?php
+}
+
+function cocolo_tag_manager_body() {
+	?>
+	<!-- Google Tag Manager (noscript) -->
+	<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KD9K4P8"
+	height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+	<!-- End Google Tag Manager (noscript) -->
+	<?php
+}
 
 function cocolo_adsense() {
 	?>
